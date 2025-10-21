747d615d78f8e6621215e7226e541e12afee426b
nlrhmkkc <nagylala2007@gmail.com>
2025-10-21 13:19:47 +0200

    asszemkesz

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import BeerCard from "./components/BeerCard"
+import BeerCard, {type BeerCardType} from "./components/BeerCard"
+import { useEffect, useState } from "react"
 
 const App = () => {
+
+  const [beer, setBeer] = useState<BeerCardType[]>([])
+  
+  useEffect(()=>{
+    fetch("/data.json")
+    .then(res =>res.json())
+    .then(data=>setBeer(data))
+  }, [])
   return (
     <div className="beerCardWrapper">
-      <BeerCard />
+      {beer.map(beer => <div className='beerCard'>
+        <img src={beer.image}/>
+        <b>{beer.name} {beer.abv} {beer.volume}</b>
+        <p>{beer.price} Ft</p>
+        <p className={beer.available? "van" : "nincs"}>{beer.available ? "Raktáron" : "Nincs raktáron"}</p>
+    </div>)}<BeerCard />
     </div>
   )
 }
--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -1,7 +1,19 @@
+type BeerCardType = {
+    image: string,
+    name: string,
+    abv: string,
+    volume: string,
+    price: number,
+    available: string
+
+  }
+
 const BeerCard = () => {
   return (
-    <div className='beerCard'>Beer Card</div>
+    <div className='beerCard'>
+    </div>
   )
 }
 
 export default BeerCard
+export type { BeerCardType }
